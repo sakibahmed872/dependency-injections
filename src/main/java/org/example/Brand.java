fcd6a6768b287518e969e065cbd8a7fb1e53d544
sakib <sakib@oneit.com.au>
2023-06-05 22:46:37 +0600

    initial commit

---
+++ src/main/java/org/example/Brand.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Brand {
+    private Car car;
+
+    public Brand(Car car)
+    {
+        this.car = car;
+    }
+
+    public void setCar(Car car) {
+        this.car = car;
+    }
+
+    public void printInfo(String info)
+    {
+        System.out.println(info);
+    }
+}
---
+++ src/main/java/org/example/Car.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Car {
+    private String title;
+
+    public Car(String title)
+    {
+        this.title = title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
